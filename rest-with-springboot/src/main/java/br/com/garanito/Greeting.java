40aaf161eacca5a5ffb9f50ae6fc3dd5f37653da
Marcos Pereira Garanito <mgaranito@hotmail.com>
2020-05-20 23:10:30 -0300

    Hello World

---
+++ rest-with-springboot/src/main/java/br/com/garanito/Greeting.java
@@ -0,0 +1,21 @@
+package br.com.garanito;
+
+public class Greeting {
+
+	private final long id;
+	private final String content;
+
+	public long getId() {
+		return id;
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public Greeting(long id, String content) {
+		this.id = id;
+		this.content = content;
+	}
+
+}
